a30c584ca74c3d600d8b8a5703d0547c898c8590
sutha06 <suthakaran.siva0601@gmail.com>
2024-04-12 18:14:03 -0400

    app typescript, but im using a mock up of flower data first to understand httpconfig

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {FlowerDetailsComponent} from "./flower-details/flower-details.component";
+import {FlowerIndexComponent} from "./flower-index/flower-index.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: FlowerIndexComponent},
+  {path: 'flowers/:id', component: FlowerDetailsComponent},
+  {path: '**', redirectTo: ''}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
