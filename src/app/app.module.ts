4ba86b5d3e65bf9e3c6ca487ea31b2ce88da24ca
sutha06 <suthakaran.siva0601@gmail.com>
2024-04-12 18:15:19 -0400

    app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { FlowerIndexComponent } from './flower-index/flower-index.component';
+import { FlowerDetailsComponent } from './flower-details/flower-details.component';
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    FlowerIndexComponent,
+    FlowerDetailsComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
