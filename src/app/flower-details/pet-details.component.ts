c47a65aeee14c6fa4b20d73764e2bba71b032bdc
sutha06 <suthakaran.siva0601@gmail.com>
2024-04-12 19:19:13 -0400

    pet details file

---
+++ src/app/flower-details/pet-details.component.ts
@@ -0,0 +1,30 @@
+import {Component, OnDestroy} from '@angular/core';
+import {Pet} from '.././pet';
+import {ActivatedRoute} from '@angular/router';
+import {PetDataService} from '../pet-data.service';
+import {Subscription} from "rxjs";
+
+@Component({
+  selector: 'app-pet-details',
+  templateUrl: './pet-details.component.html',
+  styleUrls: ['./pet-details.component.css']
+})
+export class PetDetailsComponent implements OnDestroy {
+
+  pet: Pet | undefined;
+
+  private petSub: Subscription | undefined;
+
+  constructor(petDataService: PetDataService, activatedRoute: ActivatedRoute) {
+
+    const name: string | null =  activatedRoute.snapshot.paramMap.get('name');
+    if(name != null){
+      this.petSub =
+        petDataService.getPetById(name).subscribe(pet => this.pet = pet);
+    }
+  }
+
+  ngOnDestroy(){
+    this.petSub?.unsubscribe();
+  }
+}
