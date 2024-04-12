340409b8b736d73cb9841bb08090d9f8ee3c7835
sutha06 <suthakaran.siva0601@gmail.com>
2024-04-12 19:28:28 -0400

    app file changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
-import { FlowerIndexComponent } from './flower-index/flower-index.component';
-import { FlowerDetailsComponent } from './flower-details/flower-details.component';
+import { PetIndexComponent } from './pet-index/pet-index.component';
+import { PetDetailsComponent } from './pet-details/pet-details.component';
 import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
-    FlowerIndexComponent,
-    FlowerDetailsComponent
+    PetIndexComponent,
+    PetDetailsComponent
   ],
   imports: [
     BrowserModule,
