4cb152a20d7b41992f255e18f17c5d8b3fb7a722
sutha06 <suthakaran.siva0601@gmail.com>
2024-04-12 22:48:13 -0400

    header files updated

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,14 +1,11 @@
 import {Component, Input} from '@angular/core';
 
 @Component({
-  selector: 'app-footer',
+  selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
-  @Input() name: string = 'Sheridan College';
-  today: Date;
-  constructor() {
-    this.today = new Date();
-  }
+
+  @Input() title: string = "Pet App"
 }
